5fd4218db5a4b578c22b75487c3524151dd17075
William Berbet <wiiliamb@hotmail.com>
2022-10-09 11:55:46 -0300

    home quase pronta

---
+++ src/Components/Buttons/index.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export function Buttons() {
+    return (
+        <button>
+
+        </button>
+    )
+}
+
+
+export default Buttons;
+
+
+// ==================================== STYLES ===============================
+
+const SButton = styled.div`
+    
+`
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,84 @@
+import styled from "styled-components";
+import Linkedin from '../../Assets/linkedin.png'
+import Github from '../../Assets/github.png'
+import Instagram from '../../Assets/instagram.png'
 
 
 
 export function Home() {
     return (
-        <div>
+        <>
+            <SMain>
+                <h1>Oi, eu sou o William!</h1>
+                <h3>Desenvolvedor Web/Mobile Front End</h3>
+            </SMain>
 
-        </div>
+            <SAside>
+                <p>wiiliamb@hotmail.com</p>
+            </SAside>
+
+            <SAside2>
+                <a href="https://www.linkedin.com/in/william-berbet/" target="_blank" rel="noreferrer"><img src={Linkedin} alt='' /></a>
+                <a href="https://github.com/will10iam" target="_blank" rel="noreferrer"><img src={Github} alt='' /></a>
+                <a href="https://www.instagram.com/willnasredes/" target="_blank" rel="noreferrer"><img src={Instagram} alt='' /></a>
+            </SAside2>
+        </>
     )
 }
 
 export default Home;
+
+
+// ==================================== STYLES ===============================
+
+const SMain = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    
+
+    & h1 {
+        font-size: 80px;
+    }
+
+    & h3 {
+        color: #f6336c;
+        margin-top: -30px;
+        font-size: 25px;
+        font-weight: 100;
+    }
+`
+
+const SAside = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    & p {
+        transform: rotate(90deg);
+        margin-left: 1180px;
+        margin-top: -500px;
+        opacity: 0.8;
+    }
+`
+const SAside2 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-top: -150px;
+
+    & p {
+        transform: rotate(90deg);
+        margin-right: 1150px;
+        margin-top: -180px;
+        opacity: 0.8;
+    }
+
+    & img {
+        width: 30px;
+        margin-left: 25px;
+        opacity: 0.5;
+    }
+`
